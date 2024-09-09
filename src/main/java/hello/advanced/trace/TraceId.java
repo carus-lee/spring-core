d6df0faa2ff93fc8d1cf0e210f8ecc6969835233
carus-lee <leec8.2019@gmail.com>
2024-09-10 08:33:41 +0900

    스프링핵심원리-init - 로그추적기V1 프로토타입 - 로그추적기V1 적용

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        // 생성된 UUID - 너무 길어서 일부 잘라서 사용. 로그ID라 어쩌다 중복되도 큰 문제가 안됨.
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public Boolean firstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
