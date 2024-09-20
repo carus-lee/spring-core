dec5bd35ef31c11a29e3cdc5b7143e6e3f0e13bd
carus-lee <leec8.2019@gmail.com>
2024-09-20 10:21:38 +0900

    쓰레드로컬 - 동시성문제 해결 예제코드 - 리팩토링 (FieldService 오타)

--- src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -1,13 +1,13 @@
 package hello.advanced.trace.threadlocal;
 
-import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
+public class ThreadLocalServiceTest {
 
-    private FieldService fieldService = new FieldService();
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
 
     @Test
     void test() {
@@ -15,13 +15,13 @@ public class FieldServiceTest {
 
         //람다식
         Runnable userA = () -> {
-            fieldService.logic("userA");
+            threadLocalService.logic("userA");
         };
         //기본방식
         Runnable userB = new Runnable() {
             @Override
             public void run() {
-                fieldService.logic("userB");
+                threadLocalService.logic("userB");
             }
         };
 
@@ -33,7 +33,7 @@ public class FieldServiceTest {
 
         threadA.start();
         //sleep(2000); //A실행이 완전히 끝내고 B실행 (동시성문제 X)
-        sleep(100); //A실행이 끝내기 전 B실행 (동시성문제 O) ==> 쓰레드로컬 사용해서 동시성문제 해결됨.
+        sleep(100); //A실행이 끝내기 전 B실행 (동시성문제 O)
         threadB.start();
 
         sleep(3000); //메인쓰레드 종료 대기
